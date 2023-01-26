663bf980a7ae967b4d162d41207b68e31a63329d
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2023-01-26 14:23:33 -0500

    comment out

--- lib/img.ts
+++ lib/img.ts
@@ -3,8 +3,7 @@
 import { Processor } from "unified";
 import { Node } from "unist";
 import { visit } from "unist-util-visit";
-import type { VFile } from "vfile";
-import { computeMetadata } from "./metadata";
+// import { computeMetadata } from "./metadata";
 
 /**
  * An `<img>` HAST node
@@ -43,23 +42,23 @@ function filterImageNode(node: ImageNode): boolean {
 /**
  * Adds the image's `height` and `width` to it's properties.
  */
-async function addMetadata(node: ImageNode): Promise<void> {
-  const metadata = await computeMetadata(node.properties.src);
+// async function addMetadata(node: ImageNode): Promise<void> {
+//   const metadata = await computeMetadata(node.properties.src);
 
-  node.properties.width = metadata.width;
-  node.properties.height = metadata.height;
+//   node.properties.width = metadata.width;
+//   node.properties.height = metadata.height;
 
-  if ("blurDataURL" in metadata) {
-    node.properties.blurDataURL = metadata.blurDataURL;
-  }
-}
+//   if ("blurDataURL" in metadata) {
+//     node.properties.blurDataURL = metadata.blurDataURL;
+//   }
+// }
 
 /**
  * This is a Rehype plugin that finds image `<img>` elements and adds the height and width to the properties.
  * Read more about Next.js image: https://nextjs.org/docs/api-reference/next/image#layout
  */
 export function imageMetadata(this: Processor) {
-  return async function transformer(tree: Node, file: VFile): Promise<Node> {
+  return async function transformer(tree: Node): Promise<Node> {
     if (true as any) {
       return tree;
     }
@@ -72,9 +71,9 @@ export function imageMetadata(this: Processor) {
       }
     });
 
-    for (const node of imgNodes) {
-      await addMetadata(node);
-    }
+    // for (const node of imgNodes) {
+    //   await addMetadata(node);
+    // }
 
     return tree;
   };
