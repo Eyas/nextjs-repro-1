4837344e5280de4484acf0622ed17ad7ee4415d7
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2023-01-26 14:13:47 -0500

    remove unneeded files

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,11 +1,6 @@
 import { compileMDX } from "next-mdx-remote/rsc";
 import { readFile } from "fs/promises";
 
-import { remarkHeadingId } from "remark-custom-heading-id";
-import remarkUnwrapImages from "remark-unwrap-images";
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
-import rehypePrism from "@mapbox/rehype-prism";
 import { imageMetadata } from "@/lib/img";
 
 export default async function Page(props: { params: { slug: string } }) {
@@ -13,19 +8,7 @@ export default async function Page(props: { params: { slug: string } }) {
     source: await readFile(`./blog/${props.params.slug}.mdx`),
     options: {
       mdxOptions: {
-        rehypePlugins: [
-          imageMetadata,
-          rehypeSlug as any,
-          rehypePrism,
-          // codeMeta,
-          // spanCombiner,
-        ],
-        remarkPlugins: [
-          // smartypants,
-          remarkHeadingId,
-          remarkUnwrapImages as any,
-          remarkGfm,
-        ],
+        rehypePlugins: [imageMetadata],
       },
     },
     components: {},
